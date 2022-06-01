692812cd62fd2a3bb5baa2a63797c38ad3025c0f
Juris710 <juris710.dev@gmail.com>
2022-06-01 17:49:45 +0900

    StateProviderからStateNotifierProviderに

--- lib/riverpod/hit_blow_states.dart
+++ lib/riverpod/hit_blow_states.dart
@@ -1,4 +1,17 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:wordle_test/hit_blow_state.dart';
 
-final hitBlowStatesProvider = StateProvider((ref) => <String, HitBlowState>{});
+class HitBlowStatesNotifier extends StateNotifier<Map<String, HitBlowState>> {
+  HitBlowStatesNotifier() : super({});
+
+  void clear(){
+    state = {};
+  }
+  void add(Map<String, HitBlowState> newEntries){
+    state = {...state, ...newEntries};
+  }
+}
+
+final hitBlowStatesProvider =
+    StateNotifierProvider<HitBlowStatesNotifier, Map<String, HitBlowState>>(
+        (ref) => HitBlowStatesNotifier());
