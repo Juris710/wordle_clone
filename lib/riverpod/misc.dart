3f3e41de803f98337a66ef79e0960251f427bb1a
Juris710 <juris710.dev@gmail.com>
2022-06-03 00:52:19 +0900

    入力受け付け可否Provider修正

--- lib/riverpod/misc.dart
+++ lib/riverpod/misc.dart
@@ -4,14 +4,32 @@ import 'package:wordle_test/riverpod/guess.dart';
 final answerProvider = StateProvider((ref) => "");
 
 // TODO：アニメーション実装後、前後で値を変える
-final isAnimationPlayingProvider = StateProvider((ref) => false);
+final isAnimationPlayingProvider = StateProvider.autoDispose((ref) => false);
 
-final canInputProvider = Provider((ref) {
+final isGameOverProvider = Provider.autoDispose((ref) {
+  final guesses = ref.watch(guessesNotifierProvider);
+  return (guesses.length == maxGuessTrialCount);
+});
+
+final isGameClearProvider = StateProvider.autoDispose((ref) => false);
+
+final canInputProvider = Provider.autoDispose((ref) {
   final answer = ref.watch(answerProvider);
   if (answer.isEmpty) {
     return false;
   }
   assert(answer.length == guessLength);
   final isAnimationPlaying = ref.watch(isAnimationPlayingProvider);
-  return !isAnimationPlaying;
+  if (isAnimationPlaying) {
+    return false;
+  }
+  final isGameOver = ref.watch(isGameOverProvider);
+  if (isGameOver) {
+    return false;
+  }
+  final isGameClear = ref.watch(isGameClearProvider);
+  if (isGameClear) {
+    return false;
+  }
+  return true;
 });
