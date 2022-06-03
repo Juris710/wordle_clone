d92eb3eaace390faa1a3d9558bd7f7c33cdb9d23
Juris710 <juris710.dev@gmail.com>
2022-06-03 14:14:10 +0900

    不要コード削除

--- lib/main.dart
+++ lib/main.dart
@@ -27,33 +27,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-const int maxInputLetters = 5;
-const List<String> allowedLetters = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z"
-];
