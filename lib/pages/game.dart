b16b020faf09910b3c65f16ef651be84dfcd6166
Juris710 <juris710.dev@gmail.com>
2022-06-03 14:17:50 +0900

    正解をデバッグ出力

--- lib/pages/game.dart
+++ lib/pages/game.dart
@@ -1,24 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:wordle_test/riverpod/misc.dart';
 import 'package:wordle_test/ui/guess.dart';
 import 'package:wordle_test/ui/keyboard.dart';
 
-class GamePage extends StatelessWidget {
+class GamePage extends HookConsumerWidget {
   const GamePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    useEffect(() {
+      final answer = ref.read(answerProvider);
+      print("answer is $answer");
+      return null;
+    }, const []);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Game"),
-        leading:
-          IconButton(
-            onPressed: () {
-              ScaffoldMessenger.of(context).removeCurrentSnackBar();
-              Navigator.of(context).pop();
-            },
-            icon: const Icon(Icons.arrow_back),
-          ),
-
+        leading: IconButton(
+          onPressed: () {
+            ScaffoldMessenger.of(context).removeCurrentSnackBar();
+            Navigator.of(context).pop();
+          },
+          icon: const Icon(Icons.arrow_back),
+        ),
       ),
       body: Center(
         child: Column(
