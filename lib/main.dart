69551951a2ac53b571e7a808869b4ece45abaceb
Juris710 <juris710.dev@gmail.com>
2022-06-04 11:01:55 +0900

    LicenseRegistryにGoogleFonts追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:wordle_clone/constants.dart';
@@ -6,6 +8,10 @@ import 'package:wordle_clone/routes.dart';
 
 void main() {
   GoogleFonts.config.allowRuntimeFetching = false;
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
   runApp(const MyApp());
 }
 
